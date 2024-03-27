ed73916e9bd92eeb4116bfbad6087cd0bc44bbaa
koyuul <shannonkyle.deleon@gmail.com>
2024-03-26 19:26:54 -0700

    Add search bar, change color palette

--- craftycart-webapp/src/components/taskbar.js
+++ craftycart-webapp/src/components/taskbar.js
@@ -51,14 +51,14 @@ export default function TaskBar() {
     return (
         <div className="fixed bottom-0 left-0 w-screen h-16 
                         flex flex-row items-center justify-around
-                        bg-slate-400 text-white">
+                        bg-[color:var(--dark-green)] text-white">
                 <TaskButton fn={buttonClicked} text="Saved Lists" imageAddr="/cart.svg" imageAlt="saved lists" />
                 <TaskButton fn={buttonClicked} text="New List" imageAddr="/newList.svg" imageAlt="new list" />
             <div>
                 <TaskButton fn={toggleMenu} text="Menu" imageAddr="/menu.svg" imageAlt="menu" />
                 <div className={`absolute w-auto p-2 mx-auto min-w-max bottom-16 -translate-x-1/4
                             rounded-md shadow-lg
-                            text-white bg-slate-400
+                            text-white
                             text-xl font-bold
                             transition-all duration-100 scale-0 origin-bottom
                             ${menuState ? 'scale-100' : ''}`}>
