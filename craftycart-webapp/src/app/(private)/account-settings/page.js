e7aefb62afe1cbe3316a4d32b34564bffe6ce462
KingSand08 <72523745+KingSand08@users.noreply.github.com>
2024-05-04 14:48:02 -0700

    Updated account settings
    - proper formatting as the figma design
    - currently backend is left blank

--- craftycart-webapp/src/app/(private)/account-settings/page.js
+++ craftycart-webapp/src/app/(private)/account-settings/page.js
@@ -30,39 +30,29 @@ export default function AccountInfo() {
         <div>
             <div className="flex flex-col  items-center h-screen ">
                 <BackButton />
-            <div className="px-14 py-20 text-7xl font-bold text-[color:var(--dark-green)]">Welcome {data.username}!</div>
-            <div className="grid grid-cols-3 gap-4 items-center bg-[color:var(--dark-green)] shadow-md rounded py-6 px-8 text-white text-lg">
-                <p className="text-lg font-bold text-2xl text-slate-100 col-span-3">Account Settings</p>
-                <p className="text-slate-100">Username:</p>
-                <p className="text-slate-200">{data.username}</p>
+            <div className="text-center px-14 pt-28 pb-12 text-7xl font-bold text-[color:var(--dark-green)]">Welcome {data.username}!</div>
+            <div className="flex flex-col  bg-[color:var(--dark-green)] shadow-md rounded py-6 px-8 text-white text-lg space-y-1.5 > * + *">
+                <p className="pb-2 text-2xl font-bold text-slate-100 self-center">Account Settings</p>
+                <hr className="w-48 h-1 mx-auto bg-white"></hr>
+                <div className="pt-2 flex flex-row items-center space-x-2 > * + *">
+                    <span className="text-slate-100 font-medium">Username: </span>
+                    <span className="text-slate-200 pr-10">{data.username}</span>
+                </div>
                 <button onClick={() => {alert("INSERT FUNCTIONAILTY HERE")}}
-                    className="shadow border rounded-full bg-white text-[color:var(--dark-green)] font-semibold">
+                    className="shadow border rounded-full px-2 py-1 bg-white text-[color:var(--dark-green)] font-semibold hover:bg-green-200">
                         Change Username
                 </button>
-            </div>
-{/*         
-            <div className="flex flex-col  bg-[color:var(--dark-green)] shadow-md rounded py-6 px-8 text-white text-lg space-y-1.5 > * + *">
-                <p className="text-lg font-bold text-2xl text-slate-100">Account Settings</p>
-                <div className="flex flex-row items-center space-x-2 > * + *">
-                    <span className="text-slate-100">Username:</span>
-                    <span className="text-slate-200 pr-10">{data.username}</span>
-                    <button onClick={() => {alert("INSERT FUNCTIONAILTY HERE")}}
-                    className="shadow border rounded-full px-2 py-1 bg-white text-[color:var(--dark-green)] font-semibold">
-                        Change Username
-                    </button>
+                <div className="pt-4 flex flex-row items-center self-center space-x-2 > * + *">
+                    <span className="text-slate-100 font-medium">Email: </span>
+                    <span className="text-slate-200 ">{data.email}</span>
                 </div>
-                <div className="flex flex-row items-center space-x-2 > * + *">
-                    <span className="text-slate-100">Email: </span>
-                    <span className="text-slate-200 pr-10">{data.email}</span>
-                    <button onClick={() => {alert("INSERT FUNCTIONAILTY HERE")}}
-                    className="shadow border rounded-full px-2 py-1 bg-white text-[color:var(--dark-green)] font-semibold">
+                <button onClick={() => {alert("INSERT FUNCTIONAILTY HERE")}}
+                    className="shadow border rounded-full px-2 py-1 bg-white text-[color:var(--dark-green)] font-semibold hover:bg-green-200">
                         Change Email
-                    </button>
-                </div>
-            </div> */}
+                </button>
+            </div>
         </div>
-        </div>
-        
+    </div>
     )
     
 }
