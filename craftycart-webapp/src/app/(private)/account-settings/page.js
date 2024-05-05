10fead8d19955eeffb0c5a0647ad782552457e06
KingSand08 <72523745+KingSand08@users.noreply.github.com>
2024-05-04 20:56:01 -0700

    Updated account settings
    - Added delete button
    - Added some test prompts for when changing information

--- craftycart-webapp/src/app/(private)/account-settings/page.js
+++ craftycart-webapp/src/app/(private)/account-settings/page.js
@@ -28,7 +28,7 @@ export default function AccountInfo() {
 
     return(
         <div>
-            <div className="flex flex-col  items-center h-screen ">
+            <div className="flex flex-col items-center h-screen ">
                 <BackButton />
             <div className="text-center px-14 pt-28 pb-12 text-7xl font-bold text-[color:var(--dark-green)]">Welcome {data.username}!</div>
             <div className="flex flex-col  bg-[color:var(--dark-green)] shadow-md rounded py-6 px-8 text-white text-lg space-y-1.5 > * + *">
@@ -38,7 +38,7 @@ export default function AccountInfo() {
                     <span className="text-slate-100 font-medium">Username: </span>
                     <span className="text-slate-200 pr-10">{data.username}</span>
                 </div>
-                <button onClick={() => {alert("INSERT FUNCTIONAILTY HERE")}}
+                <button onClick={() => {changeUsername()}}
                     className="shadow border rounded-full px-2 py-1 bg-white text-[color:var(--dark-green)] font-semibold hover:bg-green-200">
                         Change Username
                 </button>
@@ -46,14 +46,40 @@ export default function AccountInfo() {
                     <span className="text-slate-100 font-medium">Email: </span>
                     <span className="text-slate-200 ">{data.email}</span>
                 </div>
-                <button onClick={() => {alert("INSERT FUNCTIONAILTY HERE")}}
+                <button onClick={() => {changeEmail()}}
                     className="shadow border rounded-full px-2 py-1 bg-white text-[color:var(--dark-green)] font-semibold hover:bg-green-200">
                         Change Email
                 </button>
             </div>
+            <button onClick={() => {deleteAccount()}}
+                    className="shadow border rounded-full mt-10 px-10 py-3 bg-red-800 text-slate-200 font-semibold hover:bg-red-700 hover:font-bold hover:text-white">
+                        Delete Account
+                </button>
         </div>
     </div>
     )
     
+    function changeUsername(){
+        let newUsername = prompt ("Put in your new username:");
+        if (newUsername != "") {
+            data.username = newUsername;            
+            location.reload()
+        }
+    }
+
+    function changeEmail(){
+        let newEmail = prompt ("Put in your new email:");
+        if (newEmail != "") {
+            data.email = newEmail;            
+            location.reload()
+        }
+    }
+    
+    function deleteAccount(){
+        var result = confirm('Are you sure you want to delete your account?');
+        if (result == false){
+            event.preventDefault();
+        }
+    }
 }
 
