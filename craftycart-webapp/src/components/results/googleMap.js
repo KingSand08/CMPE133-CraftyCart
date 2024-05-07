cf0f4507460bf8cdd7f373a085275b202c260a58
KingSand08 <72523745+KingSand08@users.noreply.github.com>
2024-05-06 20:24:17 -0700

    google maps

---
+++ craftycart-webapp/src/components/results/googleMap.js
@@ -0,0 +1,16 @@
+import { Loader } from "@googlemaps/js-api-loader"
+
+const loader = new Loader({
+  apiKey: "YOUR_API_KEY",
+  version: "weekly",
+  ...additionalOptions,
+});
+
+loader.load().then(async () => {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+});
--- craftycart-webapp/src/components/results/mapWindow.js
+++ craftycart-webapp/src/components/results/mapWindow.js
@@ -1,35 +1,48 @@
 import Image from "next/image";
 import Spinner from "@/components/spinner"
+import { React, useState, useCallback } from 'react'
+import { Loader } from "@googlemaps/js-api-loader"
 
-export default function MapWindow({storeList, loading }) {
+export default function MapWindow({ storeList, loading }) {    
+    
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
 
 
     return (
         <div className="flex align-middle justify-center  ">
-            
-            <div className=" max-w-96 border-4 border-[color:var(--light-green)] border-solid rounded-lg">
-
-            { !loading ? 
-                <>
-                {/* // This is just a placeholder, put the map code here */}
-                <Image
-                    src="/map_placeholder.png"
-                    alt="map"
-                    className="rounded-lg"
-                    width={517}
-                    height={483}
-                    priority
-                    />
-
-
-
-                
-                </>
-                :
-                <div className="m-10 ">
-                    <Spinner/>
-                </div>
-            }
+            <div className=" max-w-250 border-4 border-[color:var(--light-green)] border-solid rounded-lg">
+                { !loading ? 
+                    <div className="w-52 h-52"> 
+                    {() => initMap}
+                    </div>
+                    :
+                    <div className="m-10 ">
+                        <Spinner/>
+                    </div>
+                }
             </div>
         </div>
     )
