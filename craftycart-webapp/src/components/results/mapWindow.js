76f1ab615b14a09d1b661b81eed383a2deb44664
KingSand08 <72523745+KingSand08@users.noreply.github.com>
2024-05-06 20:33:40 -0700

    working gmap

--- craftycart-webapp/src/components/results/mapWindow.js
+++ craftycart-webapp/src/components/results/mapWindow.js
@@ -1,43 +1,37 @@
 import Image from "next/image";
 import Spinner from "@/components/spinner"
 import { React, useState, useCallback } from 'react'
-import { Loader } from "@googlemaps/js-api-loader"
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
-export default function MapWindow({ storeList, loading }) {    
+export default function MapWindow({storeList, loading }) {    
+
+    const containerStyle = {
+        width: '500px',
+        height: '500px'
+    };
     
-
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
-}
-
-initMap();
-
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyCQv6vwnEIicjDnNZXLUnBYRpc9o-8uwSs"
+    })
+    
+    const center = {lat: -34.397, lng: 150.644};
+    const zoom = 8;
 
     return (
         <div className="flex align-middle justify-center  ">
             <div className=" max-w-250 border-4 border-[color:var(--light-green)] border-solid rounded-lg">
                 { !loading ? 
-                    <div className="w-52 h-52"> 
-                    {() => initMap}
-                    </div>
+                    isLoaded ? (
+                        <GoogleMap
+                            mapContainerStyle={containerStyle}
+                            center={center}
+                            zoom={zoom}
+                        >
+                            { /* Child components, such as markers, info windows, etc. */ }
+                            <></>
+                        </GoogleMap>
+                    ) : <></>
                     :
                     <div className="m-10 ">
                         <Spinner/>
