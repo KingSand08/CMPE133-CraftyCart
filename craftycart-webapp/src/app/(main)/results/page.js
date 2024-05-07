b8f11604aca1eb290920f18967896e889ae5f0c7
KingSand08 <72523745+KingSand08@users.noreply.github.com>
2024-05-06 23:02:27 -0700

    update

--- craftycart-webapp/src/app/(main)/results/page.js
+++ craftycart-webapp/src/app/(main)/results/page.js
@@ -14,7 +14,7 @@ export default function Home() {
     // the next id to be assigned to a new entry
     const [nextId, setNextId] = useState(0);
     // the local list of store results
-    const [storeList, setStoreList] = useState([]);
+    // const [storeList, setStoreList] = useState([]);
 
     // state to hold any errors going on
     const [errMessage, setErrMessage] = useState(null);
@@ -22,25 +22,30 @@ export default function Home() {
     // for routing
     const router = useRouter();
 
-   
+    // assume in-order (first: by missing, THEN by price)
+    const storeList = [
+      { index: "0", lat: 37.33444824517471, lng: -121.87967440588237, title: "Student Union" },
+      { index: "1", lat: 38.336928941399286, lng: -121.87635178390906, title: "Philz Coffee" },
+      { index: "2", lat: 37.33375018216817, lng: -121.8848188578274, title: "House of Bagles" },
+      { index: "3", lat: 37.33375018216817, lng: -121.8848188578274, title: "House of Bagles" },
+      { index: "4", lat: 37.33375018216817, lng: -121.8848188578274, title: "House of Bagles" }];
+      
+  // useEffect(() => {
 
-
-    useEffect(() => {
-
-        axios.get('/api/results').then(
-          (res) => {
-              console.log (res.data);
-              setStoreList(res.data.topStores);
-          }
-        ).catch(function (error) {
-          console.log("ERROR: " + error);
-          setErrMessage("Error fetching results for list");
-        }).finally(() => {
-          setLoading(false);
-        });
+    //     axios.get('/api/results').then(
+    //       (res) => {
+    //           console.log (res.data);
+    //           setStoreList(res.data.topStores);
+    //       }
+    //     ).catch(function (error) {
+    //       console.log("ERROR: " + error);
+    //       setErrMessage("Error fetching results for list");
+    //     }).finally(() => {
+    //       setLoading(false);
+    //     });
         
         
-    }, []);
+    // }, []);
   
   return (
     <main className="w-full h-full">
