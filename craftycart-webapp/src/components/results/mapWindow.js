f116334bbbdbfb814c9ddc763613f3270c8af6e2
KingSand08 <72523745+KingSand08@users.noreply.github.com>
2024-05-08 14:43:36 -0700

    Made location precise (#20)
    - added anchor to personal location icon for higher accuracy
    - removed distracting comments

--- craftycart-webapp/src/components/results/mapWindow.js
+++ craftycart-webapp/src/components/results/mapWindow.js
@@ -10,10 +10,10 @@ export default function MapWindow({storeList, loading }) {
         width: '80vw',
         position: 'relative',
         height: '40vw',
-        maxWidth: '800px', // Maximum width
-        maxHeight: '500px', // Maximum height
-        minHeight: '300px', // Minimum height
-        margin: 'auto', // Center the container horizontally
+        maxWidth: '800px',
+        maxHeight: '500px',
+        minHeight: '300px',
+        margin: 'auto'
     };
 
     const { isLoaded } = useJsApiLoader({
@@ -22,10 +22,9 @@ export default function MapWindow({storeList, loading }) {
     })
 
     const [currentPosition, setCurrentPosition] = useState(null);
-    const [hoveredMarker, setHoveredMarker] = useState(null); // Added hoveredMarker state
+    const [hoveredMarker, setHoveredMarker] = useState(null);
 
     useEffect(() => {
-        // Try HTML5 geolocation.
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
                 (position) => {
@@ -69,23 +68,22 @@ export default function MapWindow({storeList, loading }) {
 
     const markerIconStyle = {
         path: "M256 0c-141.44 0-256 114.56-256 256s256 448 256 448 256-306.56 256-448c0-141.44-114.56-256-256-256zm0 384c-58.88 0-106.667-47.787-106.667-106.667s47.787-106.667 106.667-106.667 106.667 47.787 106.667 106.667S314.88 384 256 384z",
-        fillColor: "#0DA74B", // Your desired color here
+        fillColor: "#0DA74B",
         fillOpacity: 1,
         scale: 0.055,
         strokeWeight: 2.5,
         strokeColor: "",
-        // Center the marker icon on its position
-        anchor: { x:250, y:700 },
+        anchor: { x:250, y:700 }
     };
 
     let currentPositionIcon = {
         path: "M68.23,13.49c10.44,1.49,19.79,6.36,26.91,13.48c7.29,7.29,12.23,16.93,13.58,27.68h14.17v13.58h-14.39 c-1.62,10.13-6.42,19.2-13.36,26.13c-7.11,7.11-16.47,11.99-26.91,13.48v15.04H54.65v-15.04c-10.44-1.49-19.79-6.36-26.9-13.48 c-6.94-6.94-11.74-16-13.36-26.13H0V54.65h14.16c1.35-10.75,6.29-20.39,13.58-27.68c7.11-7.11,16.46-11.99,26.9-13.48V0h13.58 V13.49L68.23,13.49z M61.44,35.41c13.95,0,25.25,11.31,25.25,25.25c0,13.95-11.31,25.25-25.25,25.25 c-13.95,0-25.25-11.31-25.25-25.25C36.19,46.72,47.49,35.41,61.44,35.41L61.44,35.41z M89,33.11c-7.05-7.05-16.8-11.42-27.56-11.42 c-10.76,0-20.51,4.36-27.56,11.42c-7.05,7.05-11.42,16.8-11.42,27.56c0,10.76,4.36,20.51,11.42,27.56 c7.05,7.05,16.8,11.42,27.56,11.42c10.76,0,20.51-4.36,27.56-11.42c7.05-7.05,11.42-16.8,11.42-27.56 C100.41,49.9,96.05,40.16,89,33.11L89,33.11z", // Use SymbolPath.CIRCLE for a circle icon
-        fillColor: '#5ECC5A', // Red color
+        fillColor: '#5ECC5A',
         fillOpacity: 1,
-        scale: 0.4, // Adjust the size of the circle
-        strokeColor: '#eafaef', // White color for the stroke
-        strokeWeight: 2, // Adjust the stroke weight
-        anchor: null
+        scale: 0.4,
+        strokeColor: '#eafaef',
+        strokeWeight: 2,
+        anchor: { x:25, y:70 }
     };
 
     const handleMarkerClick = (position) => {
@@ -98,10 +96,9 @@ export default function MapWindow({storeList, loading }) {
     };
 
     const onMarkerMouseOut = () => {
-        // Delay execution of onMarkerMouseOut by 3 seconds
         setTimeout(() => {
             setHoveredMarker(null);
-        }, 8000); // 8000 milliseconds = 3 seconds
+        }, 8000);
     };    
 
     return (
@@ -115,8 +112,6 @@ export default function MapWindow({storeList, loading }) {
                             zoom={zoom}
                             clickableIcons={true}
                         >
-                                {/* Marker for current position */}
-
                             {currentPosition && (
                                 <Marker
                                     position={currentPosition}
@@ -129,12 +124,9 @@ export default function MapWindow({storeList, loading }) {
                                     key={index}
                                     position={marker.position}
                                     title={marker.title}
-                                    // Set green color for the first marker
                                     icon={markerIconStyle}
                                     draggable={false}
-                                    // label={marker.title}
-                                    // ENABLE WHEN MARKER CLICKED IT WILL OPEN A GOOGLE MAPS LOCATION GIVEN LAT AND LNG 
-                                    onClick={() => handleMarkerClick(marker.position)} // Open Google Maps on marker click
+                                    onClick={() => handleMarkerClick(marker.position)}
                                     onMouseOver={() => onMarkerMouseOver(index)}
                                     onMouseOut={() => onMarkerMouseOut(index)}
 
